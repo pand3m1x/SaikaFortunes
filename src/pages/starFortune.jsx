33128a372041d6d75f62930acc7a937f03e4a0dd
TheRedQueen <pand3m1x@gmail.com>
2026-07-25 13:39:49 -0400

    all fortune pages have been updated

--- src/pages/starFortune.jsx
+++ src/pages/starFortune.jsx
@@ -7,27 +7,23 @@ import TeaFlavor from "../components/TeaFlavor";
 function StarFortune() {
   return (
     <>
-      <div className="fortune-container" style={{ display: 'flex', 
-                                           flexDirection: 'column', 
-                                           alignItems: 'center', 
-                                           gap: '10px', 
-                                           border: '1px solid green' }}>
-        <div className="main-fortune" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', border: '1px solid red' }}>
+      <div className="fortune-container" >
+        <div className="main-fortune" >
           <h1>Star Fortune</h1>
           <img src={blessStar} alt="Blessed Star" style={{ width: '200px', height: 'auto' }} />
           <p>Your star fortune goes here.</p>
         </div>
          <div className="quarter-grid">
-          <div id="tarot-card" style={{ border: '1px solid lightblue' }}>
+          <div className="fortune-card" >
             <TarotCard></TarotCard>
           </div>
-          <div id="moon-phase" style={{ border: '1px solid lightblue' }}>
+          <div className="fortune-card" >
             <MoonPhase></MoonPhase>
           </div>
-          <div id="tea-taste" style={{ border: '1px solid lightblue' }}>
+          <div className="fortune-card" >
             <TeaFlavor></TeaFlavor>
           </div>
-          <div id="cat-marking" style={{ border: '1px solid lightblue' }}>
+          <div className="fortune-card" >
             <CatMarking></CatMarking>
           </div>
         </div>
