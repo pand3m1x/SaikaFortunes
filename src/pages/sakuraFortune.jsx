833fcb8faa98e71589b1d8147213988519a87966
TheRedQueen <pand3m1x@gmail.com>
2026-07-25 13:14:49 -0400

    moved all fortune page styling to index css, added margins and other formating styling

--- src/pages/sakuraFortune.jsx
+++ src/pages/sakuraFortune.jsx
@@ -7,27 +7,23 @@ import TeaFlavor from "../components/TeaFlavor";
 function SakuraFortune() {
   return (
   <>
-    <div className="fortune-container" style={{ display: 'flex', 
-                                         flexDirection: 'column', 
-                                         alignItems: 'center', 
-                                         gap: '10px', 
-                                         border: '1px solid green' }}>
-      <div className="main-fortune" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', border: '1px solid red' }}>
+    <div className="fortune-container" >
+      <div className="main-fortune" >
         <h1>Sakura Fortune</h1>
         <img src={blessSakura} alt="Blessed Sakura" style={{ width: '200px', height: 'auto' }} />
         <p>Your sakura fortune goes here.</p>
       </div>
         <div className="quarter-grid">
-          <div id="tarot-card" style={{ border: '1px solid lightblue' }}>
+          <div className="fortune-card" >
             <TarotCard></TarotCard>
           </div>
-          <div id="moon-phase" style={{ border: '1px solid lightblue' }}>
+          <div className="fortune-card" >
             <MoonPhase></MoonPhase>
           </div>
-          <div id="tea-taste" style={{ border: '1px solid lightblue' }}>
+          <div className="fortune-card" >
             <TeaFlavor></TeaFlavor>
           </div>
-          <div id="cat-marking" style={{ border: '1px solid lightblue' }}>
+          <div className="fortune-card" >
             <CatMarking></CatMarking>
           </div>
         </div>
