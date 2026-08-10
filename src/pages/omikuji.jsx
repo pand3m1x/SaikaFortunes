477ee02aa803120a8a58c6ab2b1d74d7e7345536
TheRedQueen <pand3m1x@gmail.com>
2026-08-10 18:56:41 -0400

    added handle draw fortune to omikuji page

--- src/pages/omikuji.jsx
+++ src/pages/omikuji.jsx
@@ -1,16 +1,42 @@
+import { useNavigate } from 'react-router-dom';
+
 import omikujiIMG from '../assets/omikuji image 1.png'
 
 function Omikuji() {
+
+  const navigate = useNavigate();
+
+  function getRandomFortunePage() {
+    const roll = Math.random();
+
+     if (roll < 0.50) {
+      return "/sakurafortune";       // 0.00–0.50 → 50%
+    } else if (roll < 0.75) {
+      return "/cloudfortune";        // 0.50–0.75 → 25%
+    } else if (roll < 0.90) {
+      return "/starfortune";         // 0.75–0.90 → 15%
+    } else {
+      return "/moonfortune";         // 0.90–1.00 → 10%
+    }
+  }
+
+  function handleDrawFortune() {
+    const destination = getRandomFortunePage();
+    navigate(destination);
+  }
+
   return (
+
     <div className="fortune-container">
-      <h1>Omikuji</h1>
-      <p className="omikuji-p">Welcome to the Omikuji page! </p>
+      <h1>Saika Fortune Teller</h1>
+      <p className="omikuji-p">Welcome to the Saika Cafe Omikuji page! </p>
       <p className="omikuji-p">Here you can draw your fortune.</p>
       <img id="omikuji-image" src={omikujiIMG} alt="Japanese Omikuji fortune box" />
       <p className="omikuji-p" id="italic-p">Click the button below to draw your fortune!</p>
-      <button>Draw Fortune</button>
+      <button onClick={handleDrawFortune}>Draw Fortune</button>
       {/* Add your Omikuji functionality here */}
     </div>
+
   );
 }
 
