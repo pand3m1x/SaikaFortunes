41ff9020779f8e6b9bf15f53aede24d2761c51e0
TheRedQueen <pand3m1x@gmail.com>
2026-08-10 16:38:15 -0400

    added basic visual structure to new Omikuji page, need logic next

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // import { useState } from 'react'
 import { Routes, Route } from "react-router-dom";  
 
+import Omikuji from "./pages/omikuji.jsx";
 import StarFortune from "./pages/starFortune.jsx";
 import SakuraFortune from "./pages/sakuraFortune.jsx";
 import MoonFortune from "./pages/moonFortune.jsx";
@@ -12,6 +13,7 @@ function App() {
   return (
     <>
       <Routes>
+        <Route path="/" element={<Omikuji />}></Route>
         <Route path="/starfortune" element={<StarFortune />}></Route>
         <Route path="/sakurafortune" element={<SakuraFortune />}></Route>
         <Route path="/moonfortune" element={<MoonFortune />}></Route>
---
+++ src/pages/omikuji.jsx
@@ -0,0 +1,15 @@
+
+
+function Omikuji() {
+  return (
+    <div className="fortune-container">
+      <h1>Omikuji</h1>
+      <p>Welcome to the Omikuji page! </p>
+      <p>Here you can draw your fortune.</p>
+      <button>Draw Fortune</button>
+      {/* Add your Omikuji functionality here */}
+    </div>
+  );
+}
+
+export default Omikuji;
