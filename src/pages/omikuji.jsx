475bff9bc7b4364018910eed17ed133ef315c81b
TheRedQueen <pand3m1x@gmail.com>
2026-08-10 16:54:34 -0400

    Added image and styled more in css, next is button logic

--- src/pages/omikuji.jsx
+++ src/pages/omikuji.jsx
@@ -1,11 +1,13 @@
-
+import omikujiIMG from '../assets/omikuji image 1.png'
 
 function Omikuji() {
   return (
     <div className="fortune-container">
       <h1>Omikuji</h1>
-      <p>Welcome to the Omikuji page! </p>
-      <p>Here you can draw your fortune.</p>
+      <p className="omikuji-p">Welcome to the Omikuji page! </p>
+      <p className="omikuji-p">Here you can draw your fortune.</p>
+      <img id="omikuji-image" src={omikujiIMG} alt="Japanese Omikuji fortune box" />
+      <p className="omikuji-p" id="italic-p">Click the button below to draw your fortune!</p>
       <button>Draw Fortune</button>
       {/* Add your Omikuji functionality here */}
     </div>
